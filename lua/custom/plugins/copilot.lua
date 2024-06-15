9c6af36b7d410fd83489e7d64f86848309d387d2
denis-akopov <denis.akopov@meyersound.com>
2024-06-15 16:14:03 +0200

    added copiliot, removed delve plugin for golang

---
+++ lua/custom/plugins/copilot.lua
@@ -0,0 +1,30 @@
+return {
+
+  -- custom config which piggybacks on the copilot extras in lazy.lua.
+  {
+    'zbirenbaum/copilot.lua',
+    dependencies = {
+      'hrsh7th/nvim-cmp',
+    },
+    cmd = 'Copilot',
+    build = ':Copilot auth',
+    event = 'InsertEnter',
+    config = function()
+      require('copilot').setup {
+        panel = {
+          enabled = true,
+          auto_refresh = true,
+        },
+        suggestion = {
+          enabled = true,
+          -- use the built-in keymapping for "accept" (<M-l>)
+          auto_trigger = true,
+          accept = false, -- disable built-in keymapping
+          keymap = {
+            accept_word = '<S-tab>',
+          },
+        },
+      }
+    end,
+  },
+}
