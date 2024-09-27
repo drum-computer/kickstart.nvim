002e7492b715ee5ba2cdcc643c4f4bbc198bd051
drum-computer <densi.rhythm@gmail.com>
2024-09-27 21:26:11 +0200

    fixed some which key warnings

--- init.lua
+++ init.lua
@@ -280,19 +280,15 @@ require('lazy').setup({
       require('which-key').setup()
 
       -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+      require('which-key').add {
+        { '<leader>c', group = '[C]ode' },
+        { '<leader>d', group = '[D]ocument' },
+        { '<leader>r', group = '[R]ename' },
+        { '<leader>s', group = '[S]earch' },
+        { '<leader>w', group = '[W]orkspace' },
+        { '<leader>t', group = '[T]oggle' },
+        { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
       }
-      -- visual mode
-      require('which-key').register({
-        ['<leader>h'] = { 'Git [H]unk' },
-      }, { mode = 'v' })
     end,
   },
 
@@ -582,8 +578,8 @@ require('lazy').setup({
             '--header-insertion=never',
             '--completion-style=detailed',
             '--function-arg-placeholders',
-            '--fallback-style=llvm',
-            '-compile-commands-dir=build/Debug',
+            '--fallback-style=webkit',
+            '-compile-commands-dir=build-linux/Debug',
             '--offset-encoding=utf-16',
             -- "--query-driver=/home/denis/dev/gcc-arm-none-eabi-10-2020-q4-major/bin/arm-none-eabi-g++"
           },
