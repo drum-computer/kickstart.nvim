0453bec49f2184661f63e41bb02ba7e00015b198
drum-computer <densi.rhythm@gmail.com>
2025-12-30 18:23:54 +0100

    added rust lsp

--- init.lua
+++ init.lua
@@ -686,6 +686,7 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       vim.lsp.enable 'clangd'
+      vim.lsp.enable 'rust_analyzer'
       vim.lsp.config('clangd', {
         capabilities = capabilities,
         cmd = {
@@ -936,7 +937,7 @@ require('lazy').setup({
     -- main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'cpp', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'cpp', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'rust' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
